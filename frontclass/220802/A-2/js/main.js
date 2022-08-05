e970d24dd2f537ae0a3332118fb0e252d83baeae
gorororo <gorororo87@gmail.com>
2022-08-05 12:06:53 +0900

    home

--- frontclass/220802/A-2/js/main.js
+++ frontclass/220802/A-2/js/main.js
@@ -1,10 +1,36 @@
-$(function(){
+$(
+    function(){
     //header menu
-    $('header .mainmenu').mouseenter(function(){
-        $('.submenu, .menubg').stop().slideDown(500);
+        $('header .mainmenu').mouseenter(function(){
+            $('.submenu, .menubg').stop().slideDown(500);
+        });
+        $('header .mainmenu').mouseleave(function(){
+            $('.submenu, .menubg').stop().slideUp(500);
+        });
+    
+
+    //slide
+    const TOTAL_IMG = 2;
+    let currentIndex = 0;
+    let slidepos = 0;
+    setInterval(function(){
+        if(currentIndex < TOTAL_IMG){
+            currentIndex ++;
+        } else{
+            currentIndex = 0;
+        }
+        setPosition = currentIndex*(-1200)+'px';
+        $('.slidewrap').animate({left : (setPosition)},1000)
+
+    },3000);
+
+    //popupbox
+    $('.notice li:nth-child(1)').click(function(){
+        $('.popmenu').show();
     });
-    $('header .mainmenu').mouseleave(function(){
-        $('.submenu, .menubg').stop().slideUp(500);
+    $('.popmenu button').click(function(){
+        $('.popmenu').hide();
     });
-})
+    
+});
 
