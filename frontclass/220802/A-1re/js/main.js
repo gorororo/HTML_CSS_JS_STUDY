37adff85c3e730caa1b862a2ba0ed98db0189e28
gorororo <gorororo87@gmail.com>
2022-08-16 12:08:52 +0900

    220816home

--- frontclass/220802/A-1re/js/main.js
+++ frontclass/220802/A-1re/js/main.js
@@ -1,9 +1,33 @@
 $(function(){
     //header
-    $('header nav >ul').mouseenter(function(){
-        $('.submenu').slideDown(800)
+    $('header nav > ul').mouseenter(function(){
+        $('.submenu').stop().slideDown('slow');
+    }),
+    $('header nav > ul').mouseleave(function(){
+        $('.submenu').stop().slideUp('slow');
+    });
+    //slide
+    setInterval(function(){
+        $('.slidelist').animate({'margin-top':'-300'},function(){
+            $('.slide:first').appendTo('.slidelist')
+            $('.slidelist').css({'margin-top':'0'})
+        })
+    },3000);
+    //tab
+    $('.tabmenu h3').click(function(){
+        let idx=$(this).index()
+
+        $('.tablist >*').hide().removeClass('on')
+        $('.tablist >*').eq(idx).show().addClass('on')
+
+        $('.tabmenu h3').removeClass('on')
+        $(this).addClass('on')
+    });
+    //popup
+    $('.notice li:nth-child(1)').click(function(){
+        $('.popbox').show();
     })
-    $('header nav >ul').mouseleave(function(){
-        $('.submenu').slideUp(800)
+    $('.popup button').click(function(){
+        $('.popbox').hide();
     })
 })
---
+++ frontclass/Java_Script/training/Todolist.js
@@ -0,0 +1,58 @@
+(function(){
+    'use strict';
+    let itemList = [];
+    console.log(itemList);
+    // 객체 가져오기
+    const tagBtn =document.querySelector('#add');
+    const tagUl = document.querySelector('#itemList');
+    // tagBtn.onclick = addbtnClick;
+    tagBtn.addEventListener('click',addBtnClick);
+
+    function addBtnClick(){
+        let value = document.querySelector('#item').value;
+        document.querySelector('#item').value='';
+        if (value){
+            itemList.push(value);
+        };
+        showList();
+        bgcolorChange();
+        
+    };
+    function showList(){
+        let strTag='';
+        for(let i=0 ; i<itemList.length; i++){
+            strTag += `<li id=${i} class=close>`;
+            strTag += itemList[i];
+            strTag += '<span>X</span>';
+            strTag += '</li>';
+        }
+        tagUl.innerHTML = strTag;
+        clickRemove();
+        
+    };
+    function clickRemove(){
+        let obj = document.querySelectorAll('.close');
+        for(let i of obj){
+            i.addEventListener('click',removeList);
+        };
+    };
+    function removeList(){
+        let idx = this.getAttribute('id');
+        itemList.splice(idx,1);
+        showList();
+    }
+    function bgcolorChange(){
+        let bgColor = document.querySelectorAll('.close');
+        const colors = ['lightgreen','lightblue','lightyellow','#1abc9c','#2ecc71','#9b59b6','#34495e']
+        
+        // console.log(Idx);
+        // bgColor[0].style.backgroundColor = colors[0]
+        // bgColor[1].style.backgroundColor = colors[1]
+        
+        for (let i = 0 ; i < colors.length; i++){
+            bgColor[i].style.backgroundColor = colors[i];
+            console.log(`현재 ${i}`);
+            
+        };
+    };
+})();
