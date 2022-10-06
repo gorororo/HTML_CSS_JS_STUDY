4d8251d0e4cd33657664b364d3c66833473b872d
gorororo <gorororo87@gmail.com>
2022-10-06 17:47:19 +0900

    애플클론사이트

--- frontclass/221004_SCROLL_SEQ/JS/script.js
+++ frontclass/221004_SCROLL_SEQ/JS/script.js
@@ -1,32 +1,76 @@
-const setCurrentScene = ()=>{
+const setCurrentScene = () => {
     yOffset = window.scrollY || window.pageYOffset;
-    let total = 0
-    for (let i=0 ;i<sceneInfo.length ; i++){
+    let total = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
         total += sceneInfo[i].scrollHeight;
-        if(total >= yOffset){
+        if (total >= yOffset) {
             currentScene = i;
             break;
         }
     }
+};
+
+const calc_values = (values,ratio)=>{
+    let calc = 0;
+    calc = ratio*(values[1]-values[0])+values[0];
+
+    return calc;
 }
 
-const handlerScroll = () =>{
+const sectionAniPlay = (prevHeight) =>{
+    const currentYoffset = yOffset-prevHeight;
+    let scrollRatio= currentYoffset / sceneInfo[currentScene].scrollHeight;
+    const tagObjs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    switch(currentScene){
+        case 0 : //section-0
+        if(scrollRatio <= 0.2){
+            tagObjs.msgA.style.opacity = calc_values(values.msgA_opcity_in,scrollRatio);
+        }else{
+            tagObjs.msgA.style.opacity = calc_values(values.msgA_opcity_out,scrollRatio);
+        }
+            break;
+        case 1 : //section-1
+            break;
+        case 2 : //section-1
+            break;
+        case 3 : //section-1
+            break;
+    }
+}
+const handlerScroll = () => {
     yOffset = window.scrollY || window.pageYOffset;
-
-}
-const setSectionHeight = ()=>{
-    for(let i = 0 ; i<sceneInfo.length; i++){
+    //스크롤이 발생될 때 현재 보여지는 section 값을 가져오도록 함
+    let prevHeight = 0;
+    for(let i = 0 ; i<currentScene ; i++){
+        prevHeight += sceneInfo[i].scrollHeight;
+    }
+    if(yOffset > prevHeight + sceneInfo[currentScene].scrollHeight){
+        currentScene++;
+    }
+    if(yOffset < prevHeight){
+        currentScene--;
+    }
+    document.body.setAttribute('id',`show-section-${currentScene}`);
+    sectionAniPlay(prevHeight);
+};
+const setSecHeight = () => {
+    for (let i = 0; i < sceneInfo.length; i++) {
         sceneInfo[i].scrollHeight = sceneInfo[i].heightCnt * window.innerHeight;
-        sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+        sceneInfo[i].objs.container.style.height = sceneInfo[i].scrollHeight + 'px';
     }
     setCurrentScene();
-    console.log(currentScene);
-}
-const init = ()=>{
+};
+const init = () => {
     //각각의 section 의 height값 설정
-    setSectionHeight();
+    setSecHeight();
     //초기화
-    window.addEventListener('scroll',handlerScroll);
-}
+    console.log(currentScene);
+    window.addEventListener("scroll", ()=>{
+        //스크롤이 발생될 때 현재 보여지는 section 값을 가져오도록 함
+        handlerScroll()
+    });
+};
 
-window.addEventListener('load',init);
+window.addEventListener("load", init);
+window.addEventListener("resize",setSecHeight);
---
+++ frontclass/221004_SCROLL_SEQ/JS/variable.js
@@ -0,0 +1,49 @@
+//전역변수
+let yOffset = 0; // window.pageYOffset 정보
+let currentScene = 0; // 현재 활성화 된 section
+const sceneInfo = [
+    {//section-0
+        heightCnt : 5, //window.innerHeight 의 몇배로 height 설정
+        scrollHeight : 0,
+        objs : {
+            container : document.querySelector('#scroll-section-0'),
+            msgA : document.querySelector('#scroll-section-0 > .a'),
+            msgB : document.querySelector('#scroll-section-0 > .b'),
+            msgC : document.querySelector('#scroll-section-0 > .c'),
+            msgD : document.querySelector('#scroll-section-0 > .d'),
+        },
+        values : {
+            // msgA_opcity_in : [0,1,{start:0.2,end:0.3}],
+            // msgA_position_in : [30,0,{start:0.2,end :0.3}],
+            // msgA_opcity_out : [1,0,{start:0.3,end:0.4}],
+            // msgA_position_out : [0,-30,{start:0.3,end :0.4}],
+            msgA_opcity_in : [0,1],
+            msgA_position_in : [30,0],
+            msgA_opcity_out : [1,0],
+            msgA_position_out : [0,-30],
+        },
+        imgCount : 117,
+    },
+    {//section-1
+        heightCnt : 2,
+        scrollHeight : 0,
+        objs : {
+            container : document.querySelector('#scroll-section-1')
+        },
+        
+    },
+    {//section-2
+        heightCnt : 4,
+        scrollHeight : 0,
+        objs : {
+            container : document.querySelector('#scroll-section-2')
+        },
+    },
+    {//section-3
+        heightCnt : 5,
+        scrollHeight : 0,
+        objs : {
+            container : document.querySelector('#scroll-section-3')
+        },
+    },
+]
